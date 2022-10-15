c61973c0144c27923b957bac5557f0d0a7c49e14
Sarvang Konnur <konnursarvang@gmail.com>
2022-10-15 13:02:01 -0400

    Update reviews

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from "react";
-import AVTR1 from "../../assets/avatar1.jpg";
-import AVTR2 from "../../assets/avatar2.jpg";
-import AVTR3 from "../../assets/avatar3.jpg";
-import AVTR4 from "../../assets/avatar4.jpg";
+import EP from "../../assets/ep.png";
+import GR from "../../assets/gr.png";
+import JC from "../../assets/jc.png";
+import SS from "../../assets/ss.png";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -20,28 +20,28 @@ import { Pagination } from "swiper";
 const Reviews = () => {
   const reviewData = [
     {
-      avatar: AVTR3,
+      avatar: EP,
       name: "Erika Pilar",
       review:
         "Sarvang was a pleasure to work with and was a great team player. He communicated well with the team and never hesitated to bring up any issues to both team members and leaders. The team was highly confident with Sarvang’s quality of work and velocity. His positive attitude and energy always showed in meetings and in all of his interactions with colleagues.",
     },
     {
-      avatar: AVTR1,
-      name: "Thomas Bertin",
-      review:
-        "Do fugiat enim qui Lorem nostrud anim non pariatur occaecat dolor est incididunt ipsum. Magna ad duis velit ad. Tempor quis eiusmod laboris duis ad incididunt deserunt ex aliquip consequat quis duis in officia. Cillum elit ad duis reprehenderit incididunt occaecat veniam eu nostrud non duis. Sunt elit consectetur ea ea qui. Proident elit et ea velit aliquip adipisicing nostrud consectetur proident cillum esse nulla qui anim.",
-    },
-    {
-      avatar: AVTR2,
+      avatar: GR,
       name: "Guillaume Rieublandou",
       review:
         "If I had to describe Sarvang in two words, it would be smart and eager. Throughout his stay with us he was always interested to take on new challenges and overcame them with brio. He has been a dependable team player too, working as readily in groups as a solo, and will complete leftover work at night - because he likes it. Treat him well, he deserves it!",
     },
     {
-      avatar: AVTR4,
+      avatar: JC,
       name: "Janelle Chew",
       review:
-        "Proident elit elit enim laborum cupidatat irure fugiat dolor adipisicing duis minim velit est laboris. Sit irure enim exercitation nostrud Lorem dolor proident in minim consequat. Enim et id nulla velit irure est. Nulla labore officia in excepteur ullamco nulla adipisicing esse. Sunt consequat dolor fugiat laborum magna ex quis.",
+        "I had the pleasure of working on the same team with Sarvang for 1 year. As a product manager, I welcomed his input as he always kept the customer needs in mind. He challenges himself to learn new things and is flexible when priorities change. He is a diligent, hard worker and great communicator. His positive attitude and enthusiasm helped motivate me and the team every day.",
+    },
+    {
+      avatar: SS,
+      name: "Saba Siddiqi",
+      review:
+        "Sarvang worked with us at AI/Hub Durham College as a Research Assistant on multiple projects. The position involved working on various software, data and AI related solutions which included Python Scripts, Django/Flask Web Applications and APIs, Deployment, and any related Documentation. The position required working directly with project stakeholders and Sarvang effectively communicated and worked with team to deliver tasks timely and successfully. I have seen him go above and beyond to solve complex problems and delivery quality work. ",
     },
   ];
 
